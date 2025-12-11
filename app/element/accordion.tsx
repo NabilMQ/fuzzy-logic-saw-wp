f288b8b0137c184698409c4b40283d9027c07fb0
Nabil Mutawakkil Qisthi <nabilmq13@gmail.com>
2025-12-11 16:55:44 +0700

    enh: change font size for header on accordion

--- app/element/accordion.tsx
+++ app/element/accordion.tsx
@@ -2,7 +2,7 @@ export default function Accordion({title, children} : {title : string; children 
 
   return (
     <details className="group [&amp;_summary::-webkit-details-marker]:hidden">
-      <summary className="flex cursor-pointer font-bold text-[12px] sm:text-[14px] md:text-[16px] items-center gap-4 rounded px-3 py-2">
+      <summary className="flex cursor-pointer font-bold text-[16px] items-center gap-4 rounded px-3 py-2">
         <span>{title}</span>
 
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-down-fill transition-transform duration-300 group-open:-rotate-180" viewBox="0 0 16 16">
